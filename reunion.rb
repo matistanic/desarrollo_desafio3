bf46622d31b778dc10c974820f37735d9ab08b33
Matías <matias.mullerg@mail.udp.cl>
2020-09-17 14:42:08 -0300

    second commit

---
+++ reunion.rb
@@ -0,0 +1,52 @@
+class Appointment
+  attr_accessor :location, :purpuse, :hour, :min
+  def initialize(location, purpuse, hour, min)
+    @location = location
+    @purpuse = purpuse
+    @hour = hour
+    @min = min
+  end
+end
+
+class MonthlyAppointment < Appointment
+  attr_accessor :day
+  def initialize(location, purpuse, hour, min, day)
+    super(location, purpuse, hour, min)
+    @day = day
+  end
+
+  def occurs_on?(day)
+    @day == day
+  end
+
+  def to_s
+    puts "Reunión mensual en #{location} sobre #{purpuse} el día #{day} a la(s) #{hour}:#{min}"
+  end
+end
+
+class DailyAppointment < Appointment
+  def to_s
+   puts "Reunión diaria en #{location} sobre #{purpuse} a la(s) #{hour}:#{min}"
+ end
+ def occurs_on?(hour,min)
+   @hour == hour && @min == min
+ end
+end
+
+class OneTimeAppointment < Appointment
+  attr_accessor :day, :month, :year
+  def initialize(location, purpuse, hour, min, day, month, year)
+    super(location, purpuse, hour, min)
+    @day = day
+    @month = month
+    @year = year
+  end
+  def occurs_on?(day, month, year)
+    @day == day && @month == month && @year == year
+  end
+  def to_s
+    puts "Reunión única en #{location} sobre #{purpuse} el #{day}/#{month}/#{year} a la(s) #{hour}:#{min}"
+  end
+end
+
+puts OneTimeAppointment.new('Desafío Latam', 'Educación', 8, 15, 10, 10, 1010).to_s
